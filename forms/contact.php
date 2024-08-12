4df16bfd60fad789703f02a03fd23bf5d8072d86
jousemarquez <mquezflores@gmail.com>
2024-08-12 23:18:08 +0200

    Remove DS Store, update data, add gitignore

--- forms/contact.php
+++ forms/contact.php
@@ -1,34 +1,48 @@
 <?php
+  require_once __DIR__ . '/vendor/autoload.php';
+  use Dotenv\Dotenv;
 
-  $receiving_email_address = 'mquezflores@gmail.com';
+  $receiving_email_address = $_ENV['MAIL'];
+  $php_email_form_path = '../assets/vendor/php-email-form/php-email-form.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
+  if (file_exists($php_email_form_path)) {
+    include($php_email_form_path);
   } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
+    die('Unable to load the "PHP Email Form" Library!');
+  }
+
+  if (!class_exists('PHP_Email_Form')) {
+    die('Class "PHP_Email_Form" not found');
   }
 
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
-  
+
+  // Sanitize and validate input
+  if (!isset($_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])) {
+      die('Required fields are missing');
+  }
+
+  $name = htmlspecialchars(trim($_POST['name']));
+  $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+  $subject = htmlspecialchars(trim($_POST['subject']));
+  $message = htmlspecialchars(trim($_POST['message']));
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      die('Invalid email format');
+  }
+
   $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+  $contact->from_name = $name;
+  $contact->from_email = $email;
+  $contact->subject = $subject;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
+  $contact->add_message($name, 'From');
+  $contact->add_message($email, 'Email');
+  $contact->add_message($message, 'Message', 10);
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+  if (!$contact->send()) {
+      die('Failed to send email. Please try again later.');
+  }
 
-  echo $contact->send();
-?>
+  echo 'Message sent successfully';
