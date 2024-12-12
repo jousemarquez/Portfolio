659d4cabb9098586d87a856f3e16ba73d17c5392
jousemarquez <mquezflores@gmail.com>
2024-12-12 01:25:16 +0100

    Fix: remove unused light/dark status

--- js/main.js
+++ js/main.js
@@ -306,22 +306,6 @@
   new PureCounter();
 
   /**
-   * Dark, Light, Psycodelic Mode switcher
-   */
-
-  const body = document.querySelector('body');
-
-  document.getElementById('toggle-button2').addEventListener('click', () => {
-    document.documentElement.setAttribute('data-bs-theme', 'dark')
-    body.setAttribute('class', 'dark-theme');
-  })
-
-  document.getElementById('toggle-button').addEventListener('click', () => {
-    document.documentElement.setAttribute('data-bs-theme', 'light')
-    body.setAttribute('class', 'light-theme');
-  })
-
-  /**
    * Form checker
    */
   
